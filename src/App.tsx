1f532e26a5312cb674f847fe58583df8d3929984
suigolicious <lindasway@alumni.wfu.edu>
2024-03-19 22:27:59 -0400

    Basic GUI Skeleton of Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from 'react'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeValue, setActiveValue] = useState('');
+
+  const handleButtonClicked = (e: FormEvent<HTMLButtonElement>) => {
+    setActiveValue(e.currentTarget.innerText);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Reverse Polish Notation Calculator</h1>
+      <div className="reverse-polish-calculator">
+        <input
+          type='text'
+          placeholder='0'
+          value={activeValue}
+          onChange={() => { }} // to do for typing on keyboard
+        />
+        <div className='row'>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>CLR</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>+/-</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>/</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>x</button>
+        </div>
+        <div className='row'>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>7</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>8</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>9</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>-</button>
+        </div>
+        <div className='row'>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>4</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>5</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>6</button>
+          <button type='button' onClick={(e) => handleButtonClicked(e)}>+</button>
+        </div>
+        <div className='row-bottom'>
+          <div className='numeric-bottom-buttons'>
+            <div className='row-1'>
+              <button type='button' onClick={(e) => handleButtonClicked(e)}>1</button>
+              <button type='button' onClick={(e) => handleButtonClicked(e)}>2</button>
+              <button type='button' onClick={(e) => handleButtonClicked(e)}>3</button>
+            </div>
+            <div className='row-2'>
+              <span className='wider-button'>
+                <button type='button' onClick={(e) => handleButtonClicked(e)}>0</button>
+              </span>
+              <button type='button' onClick={(e) => handleButtonClicked(e)}>.</button>
+            </div>
+          </div>
+          <div className='taller-button'>
+            <button type='button' onClick={(e) => handleButtonClicked(e)}>ENTER</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
